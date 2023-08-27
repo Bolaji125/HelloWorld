118ccf08b1a93ecbe565160be623211105fdadaa
bolaji <ogunnaikebolaji@gmail.com>
2023-08-27 20:49:02 +0100

    Removed library.py

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from library import greet
-
+# from library import greet
+#
+# def main():
+#     greet() # importing greet() function from the "libray.py" file and executing that inside the main() function
+#
+# if __name__ =='__main__': # __name__ is a special Python variable. value of __name__ inside the main.py file will be __main__ and library inside the library.py file.
+#     main() # the if__name__ == __main__ expressions enforces a sense of a specified entry point to the programme
+#
+# # Hello, World!
 def main():
-    greet() # importing greet() function from the "libray.py" file and executing that inside the main() function
+    print("Hello, World!")
 
-if __name__ =='__main__': # __name__ is a special Python variable. value of __name__ inside the main.py file will be __main__ and library inside the library.py file.
-    main() # the if__name__ == __main__ expressions enforces a sense of a specified entry point to the programme
+
+if __name__ == '__main__':
+    main()
 
 # Hello, World!
 
