ed98a6a19b8d366dd63f26cfbad3ad01f924b63a
bolaji <ogunnaikebolaji@gmail.com>
2023-08-27 20:22:40 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from library import greet
+
+def main():
+    greet() # importing greet() function from the "libray.py" file and executing that inside the main() function
+
+if __name__ =='__main__': # __name__ is a special Python variable. value of __name__ inside the main.py file will be __main__ and library inside the library.py file.
+    main() # the if__name__ == __main__ expressions enforces a sense of a specified entry point to the programme
+
+# Hello, World!
+
+
